85ef15f76b94105da6607d58f8aca13c6bf681bb
Aaron Dong <155777538+AaronSDong@users.noreply.github.com>
2026-03-22 20:08:44 -0400

    General wave producer created

--- SineWave.py
+++ SineWave.py
@@ -1,41 +1,38 @@
 import time
 import numpy as np
 import pyaudio
-import CreateWaveShape
 
 
-class Wave:
+class SineWave:
     def __init__(self):
-        # DEBUGGING VARIABLES
-        self.sample_count = 0
-        self.last_sample = 0
-
         self.f = 440
         self.target_f = 440
         self.sample_rate = 44100
         self.volume = .5
-
         note_multiple = 1.05946
-        self.frequency_step = (note_multiple ** (1/20))
-        self.duration = 1 / self.f
+        self.frequency_step = (note_multiple ** (1/8))
+        self.steps_per_second = 100
+        self.duration = 1 / self.steps_per_second
 
         self.output_bytes = None
-        self.next_sample = 0
+        self.last_sample = 0
         self.falling_end_phase_flag = False
-        self.wave_shape = CreateWaveShape.CreateWaveShape('sine', self.sample_rate).array
 
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paFloat32, channels=1, rate=self.sample_rate, output=True)
 
     def get_next_chunk(self):
-        samples = self.wave_shape[self.next_sample::int(self.f)]
-        self.next_sample = int(self.f - ((len(self.wave_shape) - self.next_sample) % self.f))
-        if self.next_sample == self.f: self.next_sample = 0
+        # eliminate phase shift issues
+        phase_start = np.arcsin(self.last_sample)
+        if self.falling_end_phase_flag: phase_start = (np.pi / 2) + abs(phase_start - (np.pi / 2))
+        range_start = (phase_start * self.sample_rate / (2 * np.pi * self.f)) + 1
 
-        # DEBUGGING
-        #self.sample_count += len(samples)
-        #print(self.next_sample, (len(self.wave_shape) // self.f)*self.f)
-        #print(samples[0], samples[1], '\n', samples[-2], samples[-1], self.next_sample, end=' ')
+        # generate samples, note conversion to float32 array
+        phase_shifted_ranges = np.arange(range_start, (self.sample_rate * self.duration) + range_start)
+        samples = (np.sin(2 * np.pi * phase_shifted_ranges * self.f / self.sample_rate)).astype(np.float32)
+
+        # update flag if wave ends in a falling phase
+        self.falling_end_phase_flag = samples[-2] > samples[-1]
 
         self.output_bytes = (self.volume * samples).tobytes()
         self.last_sample = samples[-1]
@@ -51,28 +48,22 @@ class Wave:
 
         self.get_next_chunk()
         self.stream.write(self.output_bytes)
-        #print("Played sound for {:.8f} seconds".format(time.time() - start_time), self.sample_count)
+        #print("Played sound for {:.2f} seconds".format(time.time() - start_time))
 
     def set_target_frequency(self, frequency):
         self.target_f = frequency
 
     def set_direct_frequency(self, frequency):
         self.f = frequency
-        self.duration = 1 / self.f
 
     def slide_frequency(self):
         if self.f == self.target_f: return
 
         f_is_lower_flag = self.f < self.target_f
         self.f *= self.frequency_step
-        self.f = round(self.f)
-        self.duration = 1 / self.f
 
         # if pitch change overcompensates, set f to the target
         if f_is_lower_flag != (self.f < self.target_f): self.f = self.target_f
-
-    def set_wave_shape(self, shape):
-        self.wave_shape = CreateWaveShape.CreateWaveShape(shape, self.sample_rate).array
 
     def stop(self):
         self.stream.stop_stream()
@@ -81,10 +72,9 @@ class Wave:
 
 
 def main():
-    sinewave = Wave()
-    sinewave.set_wave_shape('square')
+    sinewave = SineWave()
     sinewave.set_target_frequency(880)
-    sinewave.play(3)
+    sinewave.play(5)
     sinewave.stop()
 
 
