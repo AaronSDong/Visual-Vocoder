61b38131c1b908824d8d7f032e6fb12c23152c50
Aaron Dong <155777538+AaronSDong@users.noreply.github.com>
2026-04-05 17:18:06 -0400

    Menu button added, background music added

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-import pygame
+from cmu_graphics import *
+from cmu_graphics.cmu_graphics import App
+
+
+def onAppStart(app):
+    app.width = 800
+    app.height = 800
+    app.buttonMenu = {
+        'cx': 200,
+        'cy': 400,
+        'w': 400,
+        'h': 200,
+        'hovered': False,
+        'imageOff': 'Menu Button.png',
+        'imageOn': 'Menu Button On.png',
+    }
+    app.buttonList = [app.buttonMenu]
+    app.bgMusic = Sound('bgMusic.mp3')
+    app.bgMusic.play(restart=True, loop=True)
+
+def redrawAll(app):
+    drawMenuButton(app, app.buttonMenu)
+    drawLabel('Visual Synthesizer!', 400, 200, font='arial', size=60)
+
+def drawMenuButton(app, button):
+    if app.buttonMenu['hovered']:
+        drawImage(button['imageOn'], button['cx'], button['cy'], width=button['w'], height=button['h'])
+    else:
+        drawImage(button['imageOff'], button['cx'], button['cy'], width=button['w'], height=button['h'])
+
+def onMouseMove(app, mouseX, mouseY):
+    for button in app.buttonList:
+        button['hovered'] = checkButtonHovered(app, app.buttonMenu, mouseX, mouseY)
+
+def checkButtonHovered(app, button, mouseX, mouseY):
+    xRange = range(button['cx'], button['cx'] + button['w'])
+    yRange = range(button['cy'], button['cy'] + button['h'])
+    return mouseX in xRange and mouseY in yRange
+
+def main():
+    runApp()
+
+if __name__ == '__main__':
+    main()
