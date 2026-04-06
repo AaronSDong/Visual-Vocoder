45bbd3df3952e0daeef13cb6342b7f9d17686a85
Aaron Dong <155777538+AaronSDong@users.noreply.github.com>
2026-04-06 15:16:01 -0400

    Branch organizing

---
+++ WaveSubGroup.py
@@ -0,0 +1,29 @@
+import Wave
+import random
+import time
+from math import floor, ceil
+
+class WaveSubGroup:
+    def __init__(self, wave_shape='sine', mono=True, freq=440, max_vol=1, delay=0.005):
+        self.sub_wave_count = 3
+        self.depth = .005  # Will range from .5ms to 100 ms
+        self.delay = delay  # Will range from .5ms to 100 ms
+        self.rate = .05  # Will range from .05 Hz to 10 Hz
+        self.dry_wet = 100  # Will range from 0-100%
+
+        self.wave_list = []
+        self.wave_list.append(Wave.Wave(wave_shape=wave_shape, mono=mono, f=freq, max_vol=max_vol))
+        freq_list = []
+
+        # determine which frequencies are needed
+        for i in range(floor(self.sub_wave_count-1)):
+            changed_freq = 'e'  # Edit low frequencies
+            freq_list.append(changed_freq)
+        for i in range(ceil(self.sub_wave_count-1)):
+            changed_freq = 'e'  # Edit high frequencies
+            freq_list.append(changed_freq)
+
+        for i in range(len(self.wave_list)):
+            delay = random.uniform(0.5, 1.5) * self.delay
+            time.sleep(delay)
+            self.wave_list.append(Wave.Wave(wave_shape=wave_shape, mono=mono, f=freq, max_vol=max_vol))
--- main.py
+++ main.py
@@ -2,55 +2,54 @@ from cmu_graphics import *
 from cmu_graphics.cmu_graphics import App
 from camera import camera
 
-
 def onAppStart(app):
     app.width = 800
     app.height = 800
-    app.buttonMenu = {
+    app.button_menu = {
         'cx': 275,
         'cy': 550,
         'w': 250,
         'h': 125,
         'hovered': False,
-        'imageOff': 'Menu Button.png',
-        'imageOn': 'Menu Button On.png',
+        'image_off': 'Menu Button.png',
+        'image_on': 'Menu Button On.png',
     }
-    app.buttonPlay = {
+    app.button_play = {
         'cx': 275,
         'cy': 350,
         'w': 250,
         'h': 125,
         'hovered': False,
-        'imageOff': 'Play Button.png',
-        'imageOn': 'Play Button On.png',
+        'image_off': 'Play Button.png',
+        'image_on': 'Play Button On.png',
     }
-    app.buttonList = [app.buttonMenu, app.buttonPlay]
-    app.bgMusic = Sound('bgMusic.mp3')
-    #app.bgMusic.play(restart=True, loop=True)
+    app.button_list = [app.button_menu, app.button_play]
+    app.bg_music = Sound('bgMusic.mp3')
+    #app.bg_music.play(restart=True, loop=True)
 
 def redrawAll(app):
-    drawButton(app, app.buttonMenu)
-    drawButton(app, app.buttonPlay)
+    draw_button(app, app.button_menu)
+    draw_button(app, app.button_play)
     drawLabel('Visual Synthesizer!', 400, 200, font='cosmic sans', size=60)
 
-def drawButton(app, button):
+def draw_button(app, button):
     if button['hovered']:
-        drawImage(button['imageOn'], button['cx'], button['cy'], width=button['w'], height=button['h'])
+        drawImage(button['image_on'], button['cx'], button['cy'], width=button['w'], height=button['h'])
     else:
-        drawImage(button['imageOff'], button['cx'], button['cy'], width=button['w'], height=button['h'])
+        drawImage(button['image_off'], button['cx'], button['cy'], width=button['w'], height=button['h'])
 
-def onMouseMove(app, mouseX, mouseY):
-    for button in app.buttonList:
-        button['hovered'] = mouseInButton(app, button, mouseX, mouseY)
+def onMouseMove(app, mouse_x, mouse_y):
+    for button in app.button_list:
+        button['hovered'] = mouse_in_button(app, button, mouse_x, mouse_y)
 
-def onMousePress(app, mouseX, mouseY):
-    if mouseInButton(app, app.buttonPlay, mouseX, mouseY):
+def onMousePress(app, mouse_x, mouse_y):
+    if mouse_in_button(app, app.button_play, mouse_x, mouse_y):
         camera()
 
-def mouseInButton(app, button, mouseX, mouseY):
-    xRange = range(button['cx'], button['cx'] + button['w'])
-    yRange = range(button['cy'], button['cy'] + button['h'])
-    return mouseX in xRange and mouseY in yRange
+def mouse_in_button(app, button, mouse_x, mouse_y):
+    x_range = range(button['cx'], button['cx'] + button['w'])
+    y_range = range(button['cy'], button['cy'] + button['h'])
+    return mouse_x in x_range and mouse_y in y_range
 
 def main():
     runApp()
